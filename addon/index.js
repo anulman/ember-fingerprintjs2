0fea245d7393ff800c8c8dc03e275e89ee11b0d2
@anulman <anulman@gmail.com>
2018-03-11 16:36:39 -0400

    feat(service): expose service at addon root, vs function alone

--- addon/index.js
+++ addon/index.js
@@ -1,12 +1,23 @@
+import Service from '@ember/service'
+import Evented from '@ember/object/evented';
+
 import RSVP from 'rsvp';
 
 const { Promise } = RSVP;
 
-export default function(options = {}) {
-  if (typeof FastBoot !== 'undefined') {
-    throw new Error('Cannot fingerprint in Fastboot');
-  }
+export default Service.extend(Evented, {
+  options: null,
+  fingerprint: null,
 
+  init() {
+    this._super(...arguments);
+
+    fingerprint(this.get('options') || {})
+      .then((fingerprint) => this.set('fingerprint', fingerprint));
+  }
+});
+
+export const fingerprint = function fingerprint(options = {}) {
   return new Promise((resolve) => {
     Fingerprint2(options).get((result, components) => {
       resolve({ result, components });
