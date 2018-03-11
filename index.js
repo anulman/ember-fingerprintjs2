46e1add21e1cd1eb4eadd0652f7d46aaa6c67932
@anulman <anulman@gmail.com>
2018-03-11 00:26:19 -0500

    feat(fingerprint2): merge && import fingerprintjs2 module

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 'use strict';
 
+const path = require('path');
+
+const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
+
 module.exports = {
-  name: 'ember-fingerprintjs2'
+  name: 'ember-fingerprintjs2',
+
+  included(app, parentAddon) {
+    let target = (parentAddon || app);
+
+    this._super.included.apply(arguments);
+
+    target.import('vendor/fingerprintjs2/fingerprint2.min.js');
+  },
+
+  treeForVendor(vendorTree) {
+    let fingerprintjsPath = path.dirname(require.resolve('fingerprintjs2'));
+    let fingerprintjsTree = new Funnel(fingerprintjsPath, {
+      files: ['fingerprint2.min.js'],
+      destDir: 'fingerprintjs2'
+    });
+
+    return mergeTrees([vendorTree, fingerprintjsTree]);
+  }
 };
